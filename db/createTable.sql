5998c5464e23795e7c535661dbfb7e018f6f99ef
peter <ken.wongkw@gmail.com>
2014-11-28 10:30:18 +0800

    Use collate utf8mb4

--- db/createTable.sql
+++ db/createTable.sql
@@ -7,7 +7,7 @@ CREATE TABLE patch_history (
   patch_history_Number INT NOT NULL default 0,
   patch_history_Action VARCHAR(10) NOT NULL default 'patch',
   patch_history_CreateDate timestamp DEFAULT CURRENT_TIMESTAMP
-) COLLATE utf8_unicode_ci;
+) COLLATE utf8mb4_unicode_ci;
 
 INSERT INTO patch_history SET patch_history_Number = 1;
 
@@ -20,7 +20,7 @@ CREATE TABLE Log (
   Log_Type CHAR(10) NOT NULL default '',
   Log_Message TEXT NOT NULL default '',
   Log_CreateDate timestamp DEFAULT CURRENT_TIMESTAMP
-) COLLATE utf8_unicode_ci;
+) COLLATE utf8mb4_unicode_ci;
 
 
 -- User --
@@ -33,7 +33,7 @@ CREATE TABLE User (
   User_Password CHAR(40) NOT NULL DEFAULT '',
   User_DisplayName VARCHAR(128) NOT NULL DEFAULT '',
   User_Level TINYINT NOT NULL DEFAULT 0
-) COLLATE utf8_unicode_ci;
+) COLLATE utf8mb4_unicode_ci;
 
 INSERT INTO User SET User_Username = 'peter', User_Password = SHA('peter'), User_DisplayName = 'Ken', User_Level = 2;
 INSERT INTO User SET User_Username = 'admin', User_Password = SHA('admin'), User_DisplayName = 'Admin', User_Level = 1;
